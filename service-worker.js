ca980a8bcb07922af95f4fe79d6878c4f5ea4894
Nazokat Abduazizova <abduazizovanazokat@gmail.com>
2026-05-24 20:52:38 +0500

    celebrate: +70% o'zgarish natija karta + tugmalar 'Keyingi haftani tuzamiz' (routine.html)

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * after first load. Caches all preview HTML files + the logo.
  */
 
-const CACHE_NAME = 'mvow-v3.8.0';
+const CACHE_NAME = 'mvow-v3.9.0';
 const ASSETS = [
   './',
   './gallery.html',
