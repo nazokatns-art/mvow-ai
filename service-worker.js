f14315991f493b47f1294ba82ab8c3a5e5df35f3
Nazokat Abduazizova <abduazizovanazokat@gmail.com>
2026-05-24 21:06:28 +0500

    celebrate phoenix: gul-o'xshash shakl olib tashlandi, 5 ta olov tili qo'yildi

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * after first load. Caches all preview HTML files + the logo.
  */
 
-const CACHE_NAME = 'mvow-v3.9.0';
+const CACHE_NAME = 'mvow-v3.9.1';
 const ASSETS = [
   './',
   './gallery.html',
